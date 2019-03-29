511a67aa87739cfd2bcf952b143d6bec1c429240
Lev Livnev <lev@liv.nev.org.uk>
2019-03-29 16:01:44 +0000

    hetzner.nix: delete some junk

--- hetzner.nix
+++ hetzner.nix
@@ -20,20 +20,5 @@ let
 
       raid / --level=1 --device=md0 --fstype=ext4 --label=root raid.1 raid.2
     '';
-    # networking.usePredictableInterfaceNames = false;
-    # networking.interfaces.eth2.macAddress = "6c:b3:11:23:52:c2";
-    # #networking.interfaces.eth1.macAddress = "e0:d5:5e:c2:cb:a6";
-    # #networking.interfaces.eth2.macAddress = "e0:d5:5e:c2:cb:a4";
-    # #networking.interfaces.eth3.macAddress = "6c:b3:11:23:52:c3";
-    # boot.kernelParams = [ "net.ifnames=0" "biosdevname=0" ];
-    # boot.initrd.extraUdevRulesCommands = with lib;
-    # let
-    #   macInterfaces = filterAttrs (name: interface: interface.macAddress != null) config.networking.interfaces;
-    #   extraUdevRules = pkgs.writeTextDir "10-mac-network.rules" (concatStrings (mapAttrsToList (name: interface: ''
-    #     ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="${interface.macAddress}", NAME="${name}"
-    #   '') macInterfaces));
-    # in ''
-    #   cp -v ${extraUdevRules}/*.rules $out/
-    # '';
   };
 }
